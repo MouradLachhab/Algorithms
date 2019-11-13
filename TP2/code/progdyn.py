d912da1b096e2dfe54df83562f3a9360d2ac8feb
MouradLachhab <mourad.laachhab@yahoo.com>
2019-11-13 16:24:55 -0500

    added cuts for progdyn

--- TP2/code/progdyn.py
+++ TP2/code/progdyn.py
@@ -2,6 +2,7 @@ import time
 
 def prog_dyn(n, cuts, prices, showCuts):
     r = [0] * n
+    path = [[]] * n
 
     for j in range(1,n):
         for i in range(j + 1):
@@ -9,7 +10,11 @@ def prog_dyn(n, cuts, prices, showCuts):
 
             if currentValue > r[j]:
                 r[j] = currentValue
+                path[j] = path[j - i] + [i]
     
+    if showCuts:
+        print(path[n - 1])
+
     return r[n - 1]
 
 def execute_prog_dyn(n, cuts, prices, showTotal, showCuts):
