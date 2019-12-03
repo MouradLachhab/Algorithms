8966888eb7364f5737c8bf00496b6a0e272ae958
MouradLachhab <mourad.laachhab@yahoo.com>
2019-12-03 14:58:43 -0500

    Worked on the report

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -17,7 +17,7 @@ struct Card
     vector<int> synergy;
     int value_with_synergy; 
 
-    bool operator < (const Card& second) const
+    bool operator<(const Card& second) const
     {
         return value_with_synergy < second.value_with_synergy;
     }
@@ -51,7 +51,7 @@ struct Deck
         sort(cards.begin(), cards.end());
     }
 
-    bool operator < (const Deck& second) const
+    bool operator<(const Deck& second) const
     {
         return value < second.value;
     }
@@ -69,51 +69,45 @@ struct Permutation
     }
 };
 
-vector<Deck*> createDecksGreedy(vector<Card*> cards, int nbDecks)
+vector<Deck> createDecksGreedy(vector<Card*> cards, int nbDecks)
 {
 	int nbCardsPerDeck = cards.size() / nbDecks;
-	vector<Deck*> decks(nbDecks);
-	vector<int> availableCards(cards.size());
-
-	for (int i = 0; i < cards.size(); ++i)
-	{
-		availableCards[i] = i;
-	}
+	vector<Deck> decks(nbDecks);
 
 	for (int i = 0; i < nbDecks; ++i)
 	{
-		decks[i] = new Deck();
-		int index = rand() % availableCards.size();
-		decks[i]->cards.push_back(cards[availableCards[index]]);
-        decks[i]->value += cards[availableCards[index]]->value;
-		availableCards.erase(availableCards.begin() + index);
+		int index = rand() % cards.size();
+		decks[i].cards.push_back(cards[index]);
+        decks[i].value += cards[index]->value;
+		cards.erase(cards.begin() + index);
 	}
 
 	bool done = false;
 	int tooManyLoops = 0;
-	while (!availableCards.empty())
+	while (!cards.empty())
 	{
-        Card* currentCard = cards[availableCards.back()];
-        availableCards.pop_back();
+        int index = rand() % cards.size();
+        Card* currentCard = cards[index];
+        cards.erase(cards.begin() + index);
 
-        int bestBump = 0;
+        int bestBump = -9999;
         int bestValue = -9999;
         int deckIndex = -1;
 
 		for (int i = 0; i < nbDecks; ++i)
 		{
-            if (decks[i]->cards.size() < nbCardsPerDeck)
+            if (decks[i].cards.size() < nbCardsPerDeck)
             {
-                int deckValue = decks[i]->value + currentCard->value;
+                int deckValue = decks[i].value + currentCard->value;
 
-                for (int j = 0; j < decks[i]->cards.size(); ++j)
+                for (int j = 0; j < decks[i].cards.size(); ++j)
                 {
-                    deckValue += decks[i]->cards[j]->synergy[currentCard->id];
+                    deckValue += decks[i].cards[j]->synergy[currentCard->id];
                 }
 
-                int currentBump = deckValue - decks[i]->value;
+                int currentBump = deckValue - decks[i].value;
 
-                if (currentBump > bestBump || deckIndex == -1)
+                if (currentBump > bestBump)
                 {
                     bestBump = currentBump;
                     bestValue = deckValue;
@@ -122,8 +116,8 @@ vector<Deck*> createDecksGreedy(vector<Card*> cards, int nbDecks)
             }
 		}
 
-        decks[deckIndex]->cards.push_back(currentCard);
-        decks[deckIndex]->value = bestValue;
+        decks[deckIndex].cards.push_back(currentCard);
+        decks[deckIndex].value = bestValue;
 	}
 
 	return decks;
@@ -175,13 +169,13 @@ void testDeck(vector<Card*> cards)
     cout << "HAHAHAH: " << deckvalue << endl;
 }
 
-int getMinValue(vector<Deck*> decks)
+int getMinValue(vector<Deck>& decks)
 {
 	int  min = 99999999;
-	for (Deck* deck : decks)
+	for (Deck deck : decks)
 	{
-		if (deck->value < min)
-			min = deck->value;
+		if (deck.value < min)
+			min = deck.value;
 	}
 
 	return min;
@@ -247,51 +241,41 @@ int main(int argc, char **argv)
     }
 
     vector<Permutation*> tabu_permutation;
-    vector<Deck*> decks_curr;
-    vector<Deck*> best_deck;
+    vector<Deck> best_deck;
     vector<Card*> cards = createCards(cardValues, synergies);
     srand(time(NULL));
 
 	int current_min = -9999;
 
-    testDeck(cards);
-
     while(1)
     {
-		decks_curr = createDecksGreedy(cards, nbDecks);
+        vector<Deck> decks_curr = createDecksGreedy(cards, nbDecks);
 
 		int newMin = getMinValue(decks_curr);
 
 		if (current_min < newMin)
 		{ 
-			cout << "New min: " << newMin << endl;
+            std::sort(decks_curr.begin(), decks_curr.end());
             for (int i = 0; i < decks_curr.size(); ++i)
             {
-                for (int j = 0; j < decks_curr[i]->cards.size(); ++j)
+                for (int j = 0; j < decks_curr[i].cards.size(); ++j)
                 {
-                    cout << decks_curr[i]->cards[j]->id << " ";
+                    cout << decks_curr[i].cards[j]->id << " ";
                 }
-
-                cout << "Deck Value: "<< decks_curr[i]->value << endl;
+                cout << endl;
+                // cout << "DECK VALUE : " << decks_curr[i].value << endl;
             }
+            // cout << "New Min: "<< newMin << endl;
 			current_min = newMin;
 			best_deck = decks_curr;
 		}
         
     }
 
-   for (auto p : best_deck)
-   {
-     delete p;
-   }
-    for (auto p : decks_curr)
-   {
-     delete p;
-   }
     for (auto p : cards)
-   {
-     delete p;
-   }
+    {
+        delete p;
+    }
 
    
 	return 0;
